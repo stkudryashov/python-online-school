e63e2b49622fef02034aebd4041de75d963fd606
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-11-17 18:22:24 +0300

    Start frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-between mt-40">
+    <div class="">
+      <p>Hello World</p>
+    </div>
+    <div class="">
+      <p>Hello World</p>
+    </div>
+    <div class="">
+      <p>Hello World</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
