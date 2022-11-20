ac9fe215be7b3b104ab887752c83a4ab65a4e0bf
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-11-20 14:48:35 +0300

    Change styles page not found

--- frontend/src/pages/PageNotFound.vue
+++ frontend/src/pages/PageNotFound.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="flex-1">
-    <div class="flex items-center justify-center mt-40 text-black">
+  <section class="flex-1 container mx-auto">
+    <div class="not-found-block">
       <h1 class="text-3xl mr-2">404 к сожалению такого у нас нет</h1>
       <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
@@ -16,5 +16,7 @@ export default {
 </script>
 
 <style scoped>
-
+.not-found-block {
+  @apply flex items-center justify-center mt-24 text-black bg-gray-100 rounded-2xl drop-shadow-xl h-48
+}
 </style>
