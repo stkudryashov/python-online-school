4a208d31717785f1043d5aa8be920c1e0cb8d6bc
Aleks Kudryashov <aleks59niki30@gmail.com>
2022-11-30 07:01:58 +0300

    Add plate for courses

--- frontend/src/pages/personal/PersonalPageCourses.vue
+++ frontend/src/pages/personal/PersonalPageCourses.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1 class="text-2xl text-green-600"></h1>
+  <div class="flex justify-center">
+    <div class="flex-1 m-8 h-64 bg-gray-100 dark:bg-gray-800 rounded-2xl drop-shadow-2xl">
+
+    </div>
+  </div>
 </template>
 
 <script>
